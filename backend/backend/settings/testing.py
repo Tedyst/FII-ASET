b7bdf86a3ae9cb3bce9a714304d1d4d4cbbc968b
Stoica Tedy <stoicatedy@gmail.com>
2024-11-02 12:46:36 +0200

    Instaleaza aplicatia in cluster si leaga aplicatia la Postgres si pune totul in Ansible pentru configuratiie automata

--- backend/backend/settings/testing.py
+++ backend/backend/settings/testing.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "profiles.User"
 
 CELERY_ALWAYS_EAGER = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
