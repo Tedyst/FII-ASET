7033a2d9247c1d55735b3f4b39faf228de1cd70a
Stoica Tedy <stoicatedy@gmail.com>
2024-11-04 08:56:46 +0200

    Add yfinance to pyproject.toml and run `pre-commit run -a`

--- backend/trading/tasks.py
+++ backend/trading/tasks.py
@@ -1,21 +1,18 @@
-
 import yfinance as yf
 from celery import shared_task
+
 from .models import Ticker
-import pandas as pd
-import requests
-import io
-import csv
+
+
 @shared_task
 def update_ticker_prices():
     tickers = Ticker.objects.all()
     for ticker in tickers:
         try:
             stock = yf.Ticker(ticker.symbol)
-            price = stock.history(period="1d").tail(1)['Close'].iloc[0]
+            price = stock.history(period="1d").tail(1)["Close"].iloc[0]
             ticker.price = price
             ticker.save()
             print(f"Updated {ticker.symbol} price to {ticker.price}")
         except Exception as e:
             print(f"Failed to update {ticker.symbol}: {e}")
-
