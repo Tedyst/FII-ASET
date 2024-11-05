8cef2eac6ffae87b8022d6afd5aa8c23ad75491b
Stoica Tedy <stoicatedy@gmail.com>
2024-11-05 09:48:14 +0200

    Added an front page generated by ChatGPT and a theme selector

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import test
+from .views import index
 
 urlpatterns = [
-    path("test", test),
+    path("", index),
 ]
--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-def test(request):
-    # return base.html template
-    return render(request, "base.html")
+def index(request):
+    return render(request, "index.html")
