ebec15d68adea9408a8ce7a9d45f47321926a390
Artur Bejenari <artur.bejenari00@gmail.com>
2024-11-06 21:52:03 +0200

    Added main page, Available Actions, Personal Actions

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from . import views
+from .views import available_actions_list, index, personal_actions_list
 
 urlpatterns = [
-    path("", index),
+    path("", index, name="index"),
+    path("personal-actions/", personal_actions_list, name="personal_actions"),
+    path("available-actions/", available_actions_list, name="available_actions"),
 ]
--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from frontend.services.actions_service import ActionsService
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def personal_actions_list(request):
+    actions_service = ActionsService()
+    pers_actions_list = actions_service.get_all_personal_actions()
+
+    return render(request, "view_personal_actions.html", {"actions": pers_actions_list})
+
+
+def available_actions_list(request):
+    actions_service = ActionsService()
+    avail_actions_list = actions_service.get_all_available_actions()
+
+    return render(
+        request, "view_available_actions.html", {"actions": avail_actions_list}
+    )
